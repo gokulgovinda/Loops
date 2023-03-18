4a3080d1e79e0dd6c612903da76efd373138ad8b
Gokul Govinda <87459046+gokulgovinda@users.noreply.github.com>
2023-03-18 22:37:41 +0530

    Update Loops.java

--- Loops.java
+++ Loops.java
@@ -1,12 +1,20 @@
 
 public class Loops {
 public static void main(String[]args) {
-	int counter=0;
-	while(counter < 100) {
-		System.out.println("Hello world");
-		counter++;
-	}
-
+// 	int counter=0;
+// 	while(counter < 100) {
+// 		System.out.println("Hello world");
+// 		counter++;
+// 	}
+// Reverse a number eg.99801
+	int  n=99801;
+		while(n>0){
+			int lastDigit=n%10;
+			System.out.print(lastDigit + " ");
+			n/=10;
+		}
+	System.out.println();
+}
 
 }
 }
